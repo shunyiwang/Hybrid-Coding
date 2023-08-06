7b815eb230951e9d35dbd67ddf1782f7184e0e7b
shunyiwang <31991290+shunyiwang@users.noreply.github.com>
2023-08-06 23:12:41 +0800

    Delete Upload directory

--- Upload/Src/ndn-thread.cpp
+++
@@ -1,8 +0,0 @@
-#include "ndn-thread.hpp"
-#include "Src/ndnputsegments.hpp"
-
-void NDNThread::run()
-{
-    ndn::segment::Put Putsegment;   //定义上传文件类的对象
-    Putsegment.PutSegment();   //调用上传文件类的上传文件块函数
-}
